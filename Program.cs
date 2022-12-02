a87fdba2d4f8b6808c161386837b142aa2ab7e3a
Kris <data.base@rambler.ru>
2022-12-02 09:46:55 +0300

    Добавление блок схемы

--- Program.cs
+++ Program.cs
@@ -10,11 +10,11 @@ void SecondArray(string[] array1, string[] array2)
     int count = 0;
     for (int i = 0; i < array1.Length; i++)
     {
-    if(array1[i].Length <= 3)
-        {
-        array2[count] = array1[i];
-        count++;
-        }
+        if(array1[i].Length <= 3)
+            {
+            array2[count] = array1[i];
+            count++;
+            }
     }
 }
 void PrintArray(string[] array)
